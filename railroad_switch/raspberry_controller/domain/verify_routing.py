5209c72fd903e5418106e0cd2cf3a2f8bd30521a
Emilien <emilienguillet@gmail.com>
2026-03-11 11:53:30 +0100

    Update verify_routing.py

--- railroad_switch/raspberry_controller/domain/verify_routing.py
+++ railroad_switch/raspberry_controller/domain/verify_routing.py
@@ -3,23 +3,20 @@ from domain.train_state import TrainState
 
 
 def is_routing_right(train: TrainState) -> bool:
-    return not (
+    return (
         (
             train.init_position == Position.REVERSE
-            and (
-                train.objective_position == Position.REVERSE
-                or train.objective_position == Position.NORMAL
-            )
+            and (train.objective_position == Position.LEAD)
         )
         or (
             train.init_position == Position.NORMAL
+            and (train.objective_position == Position.LEAD)
+        )
+        or (
+            train.init_position == Position.LEAD
             and (
                 train.objective_position == Position.NORMAL
                 or train.objective_position == Position.REVERSE
             )
-        )
-        or (
-            train.init_position == Position.LEAD
-            and train.objective_position == Position.LEAD
         )
     )
