1f87db86a6ca4c728410c616051a5f14ce708c1e
Emilien <emilienguillet@gmail.com>
2026-04-07 22:13:10 +0200

    Update calculate_real_successors.py

--- real_circuit/calculate_real_successors.py
+++ real_circuit/calculate_real_successors.py
@@ -1,121 +1,152 @@
 import json
 from pathlib import Path
-from platform import node
 
-import matplotlib.pyplot as plt
 import networkx as nx
 
 BASE_DIR = str(Path(__file__).parent.resolve())
+
+"""Extracting data from circuit.json"""
 json_file = open(rf"{BASE_DIR}\circuit.json", "r")
 data = json.load(json_file)
 json_file.close()
-list_structures = data["structures"]
-dict_graph_type = data["graphe type"]
-dict_route = data["route"]
-list_uturn = data["u-turn"]
+
+structures: list[dict[str, str | list[dict[str, str]]]] = data["structures"]
+type_by_graph_type: dict[str, list[str]] = data["graphe type"]
+entrance_exit_by_type: dict[str, list[dict[str, list[str]]]] = data["route"]
+uturn_nodes: list[str] = data["u-turn"]
+
+
+def map_successors(node_ids, props_by_node_id):
+    successor_by_node = {}
+    for node_id in node_ids:
+        successor_by_node[node_id] = []
+        id = props_by_node_id[node_id]["id"]
+        sens = props_by_node_id[node_id]["sens"]
+        add_successor(node_id, id, sens, successor_by_node)
+
+    return successor_by_node
+
+
+def handle_node_successors(node_id, structure, sens, successor_by_node):
+    successor_structures = structure["successeurs"]
+    for next_structure in successor_structures:
+        type_next_structure = next_structure["type"]
+        id_next_structure = next_structure["id"]
+        if next_structure["sens"] == sens:
+            if type_next_structure in type_by_graph_type["node"]:  # structure is a node
+                successor_by_node[node_id].append(id_next_structure + "," + sens[0])
+            else:  # structure is an edge connector
+                new_entrance_position = next_structure["successeur_position_entree"]
+                add_successor(
+                    node_id,
+                    id_next_structure,
+                    sens,
+                    successor_by_node,
+                    new_entrance_position,
+                )
+
+
+def handle_edge_connectors(
+    node_id, structure, sens, successor_by_node, entrance_position
+):
+    type_structure = structure["type"]
+    exit_positions = []
+    for exit_by_entrance in entrance_exit_by_type[type_structure]:
+        if exit_by_entrance["position_entree"] == entrance_position:
+            exit_positions = exit_by_entrance["positions_sortie"].copy()
+
+    successor_structures = structure["successeurs"]
+    for next_structure in successor_structures:
+        if (
+            next_structure["sens"] == sens
+            and next_structure["position_sortie"] in exit_positions
+        ):
+            type_next_structure = next_structure["type"]
+            id_next_structure = next_structure["id"]
+            if type_next_structure in type_by_graph_type["node"]:
+                successor_by_node[node_id].append(id_next_structure + "," + sens[0])
+
+            else:
+                new_entrance_position = next_structure["successeur_position_entree"]
+                add_successor(
+                    node_id,
+                    id_next_structure,
+                    sens,
+                    successor_by_node,
+                    new_entrance_position,
+                )
+
+
+def handle_uturn_node(node_id, structure, sens, successor_by_node):
+    id = structure["id"]
+    if id in uturn_nodes:
+        if sens == "horaire":
+            new_sens = "trigo"
+        else:
+            new_sens = "horaire"
+        successor_by_node[node_id].append(id + "," + new_sens[0])
 
 
 def add_successor(
+    node_id,
     id,
     sens,
-    position_entree=None,
+    successor_by_node,
+    entrance_position=None,
 ) -> None:
-    for structure in list_structures:
+    for structure in structures:
         if structure["id"] == id:
-            type_structure = structure["type"]
-            for potential_successor in structure["successeurs"]:
-                type_potential_successor = potential_successor["type"]
-                id_potential_successor = potential_successor["id"]
-                if position_entree is None and potential_successor["sens"] == sens:
-                    if type_potential_successor in dict_graph_type["node"]:
-                        successor_by_node[node_id].append(
-                            id_potential_successor + "," + sens[0]
-                        )
-                    else:
-                        new_id = id_potential_successor
-                        new_position_entree = potential_successor[
-                            "successeur_position_entree"
-                        ]
-                        add_successor(
-                            new_id,
-                            sens,
-                            new_position_entree,
-                        )
-
-                if position_entree is not None and potential_successor["sens"] == sens:
-                    positions_sortie = []
-                    for sorties_by_entree in dict_route[type_structure]:
-                        if sorties_by_entree["position_entree"] == position_entree:
-                            positions_sortie = sorties_by_entree["positions_sortie"]
-                    if potential_successor["position_sortie"] in positions_sortie:
-                        type_potential_successor = potential_successor["type"]
-                        id_potential_successor = potential_successor["id"]
-                        if type_potential_successor in dict_graph_type["node"]:
-                            successor_by_node[node_id].append(
-                                id_potential_successor + "," + sens[0]
-                            )
-
-                        else:
-                            new_id = id_potential_successor
-                            new_position_entree = potential_successor[
-                                "successeur_position_entree"
-                            ]
-                            add_successor(
-                                new_id,
-                                sens,
-                                new_position_entree,
-                            )
-
-            if id in list_uturn:
-                if sens == "horaire":
-                    new_sens = "trigo"
-                else:
-                    new_sens = "horaire"
-                successor_by_node[node_id].append(id + "," + new_sens[0])
+            if entrance_position is None:
+                handle_node_successors(node_id, structure, sens, successor_by_node)
+                handle_uturn_node(node_id, structure, sens, successor_by_node)
+            else:
+                handle_edge_connectors(
+                    node_id, structure, sens, successor_by_node, entrance_position
+                )
 
 
-node_ids = []
-props_by_node_id = {}
+def create_node_ids_and_props():
+    node_ids = []
+    props_by_node_id = {}
+    for structure in structures:
+        current_id = structure["id"]
+        current_type = structure["type"]
+        if current_type in type_by_graph_type["node"]:
+            for successor_structures in structure["successeurs"]:
+                current_sens = successor_structures["sens"]
+                node_id = current_id + "," + current_sens[0]
+                if node_id not in node_ids:
+                    node_ids.append(node_id)
+                    props_by_node_id[node_id] = {"id": current_id, "sens": current_sens}
 
-for dict_structure in list_structures:
-    current_id = dict_structure["id"]
-    current_type = dict_structure["type"]
-    if current_type in dict_graph_type["node"]:
-        for dict_successeur in dict_structure["successeurs"]:
-            current_sens = dict_successeur["sens"]
-            node_id = current_id + "," + current_sens[0]
-            if node_id not in node_ids:
-                node_ids.append(node_id)
-                props_by_node_id[node_id] = {"id": current_id, "sens": current_sens}
+    for structure in structures:
+        for successor_structures in structure["successeurs"]:
+            current_sens = successor_structures["sens"]
+            successeur_id = successor_structures["id"]
+            successeur_type = successor_structures["type"]
+            if successeur_type in type_by_graph_type["node"]:
+                node_id = successeur_id + "," + current_sens[0]
+                if node_id not in node_ids:
+                    node_ids.append(node_id)
+                    props_by_node_id[node_id] = {
+                        "id": successeur_id,
+                        "sens": current_sens,
+                    }
 
-for dict_structure in list_structures:
-    for dict_successeur in dict_structure["successeurs"]:
-        current_sens = dict_successeur["sens"]
-        successeur_id = dict_successeur["id"]
-        successeur_type = dict_successeur["type"]
-        if successeur_type in dict_graph_type["node"]:
-            node_id = successeur_id + "," + current_sens[0]
-            if node_id not in node_ids:
-                node_ids.append(node_id)
-                props_by_node_id[node_id] = {"id": successeur_id, "sens": current_sens}
+    for id in uturn_nodes:
+        node_id_1 = id + ",t"
+        node_id_2 = id + ",h"
+        if node_id_1 not in node_ids:
+            node_ids.append(node_id_1)
+            props_by_node_id[node_id_1] = {"id": id, "sens": "trigo"}
+        if node_id_2 not in node_ids:
+            node_ids.append(node_id_2)
+            props_by_node_id[node_id_2] = {"id": id, "sens": "horaire"}
+    return node_ids, props_by_node_id
 
-for id in list_uturn:
-    node_id_1 = id + ",t"
-    node_id_2 = id + ",h"
-    if node_id_1 not in node_ids:
-        node_ids.append(node_id_1)
-        props_by_node_id[node_id_1] = {"id": id, "sens": "trigo"}
-    if node_id_2 not in node_ids:
-        node_ids.append(node_id_2)
-        props_by_node_id[node_id_2] = {"id": id, "sens": "horaire"}
 
-successor_by_node = {}
-for node_id in node_ids:
-    successor_by_node[node_id] = []
-    id = props_by_node_id[node_id]["id"]
-    sens = props_by_node_id[node_id]["sens"]
-    add_successor(id, sens)
-G = nx.DiGraph(successor_by_node)
-nx.write_gexf(G, rf"{BASE_DIR}\graph.gexf")
-
-print(successor_by_node)
+if __name__ == "__main__":
+    node_ids, props_by_node_id = create_node_ids_and_props()
+    successor_by_node = map_successors(node_ids, props_by_node_id)
+    G = nx.DiGraph(successor_by_node)
+    nx.write_gexf(G, rf"{BASE_DIR}\graph.gexf")
