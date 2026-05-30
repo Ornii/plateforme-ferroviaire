a3f50d6c1fca133dfb31340436838f72e0a3935b
Emilien <emilienguillet@gmail.com>
2026-05-30 10:09:22 +0200

    remove hardcoded usb port

--- railroad_switch_modbus/raspberry_controller/communication/arduino_modbus_bridge.py
+++ railroad_switch_modbus/raspberry_controller/communication/arduino_modbus_bridge.py
@@ -1,12 +1,13 @@
 from pymodbus.client import ModbusSerialClient
 
 MODBUS_ID = 0x08
+MODBUS_PORT = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"
 
 
 class ArduinoModbusBridge:
-    def __init__(self, id: int = MODBUS_ID) -> None:
+    def __init__(self, id: int = MODBUS_ID, port: str = MODBUS_PORT) -> None:
         self.client = ModbusSerialClient(
-            port="/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0",
+            port=port,
             baudrate=9600,
             bytesize=8,
             parity="N",
