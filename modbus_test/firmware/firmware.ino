f0fbcc98f8c31772e08ce6c9f9e85be2feabb587
Emilien <emilienguillet@gmail.com>
2026-06-04 14:14:21 +0200

    create file tests for modbus

---
+++ modbus_test/firmware/firmware.ino
@@ -0,0 +1,29 @@
+#include <ArduinoRS485.h>
+#include <ArduinoModbus.h>
+
+const int GREEN_LED_PIN = 2;
+constexpr uint16_t COIL_SIGNAL = 0;
+const uint8_t MODBUS_ID = 0x08;
+const unsigned long BAUDRATE = 9600;
+const int LOOP_DELAY_MS = 50;
+
+void setup() {
+  pinMode(GREEN_LED_PIN, OUTPUT);
+
+  if (!ModbusRTUServer.begin(MODBUS_ID, BAUDRATE)) {
+    while (true) {
+        delay(1);
+    }
+  }
+
+  ModbusRTUServer.configureCoils(0, 1);
+}
+
+void loop() {
+  if (ModbusRTUServer.poll()) {
+      bool green = ModbusRTUServer.coilRead(COIL_SIGNAL);
+      digitalWrite(GREEN_LED_PIN, green);
+  }
+
+  delay(LOOP_DELAY_MS);
+}
